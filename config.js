f1d0f38dea7ab78a06d0a067f62c329dca4899b4
windimitrib-arch <windimitrib@gmail.com>
2026-02-05 23:57:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Gaby 💝 Mon Ange 💝",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Veux-tu être ma Valentine ? 💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
